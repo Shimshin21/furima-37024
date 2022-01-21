c874f4e0543d4fbfa29eec085233eff291605181
Shimshin21 <shin.11.pei@gmail.com>
2022-01-21 20:18:59 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+        has_many :items
+        has_many :buyers
+
         validates :nickname,  presence: true
-        validates :email,     presence: true, uniqueness: true
-        validates :encrypted_password, presence: true
+        validates :email,     presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+        validates :encrypted_password, presence: true, format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{6,}/}
         validates :family_name, presence: true
         validates :first_name, presence: true
         validates :family_name_kana, presence: true
         validates :first_name_kana, presence: true
         validates :birth, presence: true
-
 end
+
