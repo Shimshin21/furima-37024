ce6173136733338ada0a5097161939076157c644
Shimshin21 <shin.11.pei@gmail.com>
2022-01-25 19:47:03 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ApplicationRecord
 
         validates :family_name, presence: true
         validates :first_name, presence: true
-        validates :family_name_kana, presence: true
-        validates :first_name_kana, presence: true
+        validates :family_name_kana, presence: true, format: { :with KATAKANA_REGEXP }
+        validates :first_name_kana, presence: true, format: { :with KATAKANA_REGEXP }
         validates :birth, presence: true
 end
 
