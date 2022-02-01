49e99e279e16fa2496af0408414ada1db9b3398f
Shimshin21 <shin.11.pei@gmail.com>
2022-02-01 18:12:00 +0900

    Active Storage の導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,16 @@ class Item < ApplicationRecord
   has_one    :buyer
   belongs_to :user
   has_one_attached :image
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
+
+
+
 
 #belongs_to_active_hash :category
 #belongs_to_active_hash :item_condition
