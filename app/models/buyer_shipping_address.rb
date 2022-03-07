7fb122e9a0b5fa11863430fd2d506a781bfa5201
Shimshin21 <shin.11.pei@gmail.com>
2022-03-07 19:15:06 +0900

    buyer_shippng_addressモデルのテストコード

--- app/models/buyer_shipping_address.rb
+++ app/models/buyer_shipping_address.rb
@@ -11,12 +11,11 @@ class BuyerShippingAddress
 
     validates :token, presence: true
 
-    validates :post_code, presence: true, format: {with: /\A\d{3}[-]\d{4}\z/, message: "post_code is invalid. Include hyphen(-)"}
+    validates :post_code, presence: true, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city, presence: true
     validates :address, presence: true
-    validates :tell_number, presence: true,  format: {with: /\A[0-9]+\z/, message: "tell_number is invalid."}
-    validates :tell_number, presence: true, format: {with: /\A\d{10,11}\z/, message: "tell_number of characters is invalid."}
-    
+    validates :tell_number, presence: true,  format: {with: /\A[0-9]+\z/}
+    validates :tell_number, presence: true, format: {with: /\A\d{10,11}\z/, message: "of characters is invalid."}
     
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
@@ -27,20 +26,3 @@ class BuyerShippingAddress
     Buyer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, tell_number: tell_number, shipping_address_id: shipping_address.id )
   end
 end
-
-
-
-#shipping_addresses(マイグレーションファイルのバリデーション)
-      #t.references :item, foreign_key: true
-      #t.references :user, foreign_key: true
-
-#buyer (マイグレーションファイルのバリデーション)
-      #t.string     :post_code,   null: false
-      #t.integer    :prefecture_id,    null: false
-      #t.string     :city,    null: false
-      #t.string     :address,    null: false 
-      #t.string     :building_name
-      #t.string     :tell_number,    null: false  
-      
-      #t.references :shipping_address, null: false, foreign_key: true
-
--- spec/factories/buyer_shipping_addresses.rb
+++ spec/factories/buyer_shipping_addresses.rb
@@ -1,13 +1,20 @@
 FactoryBot.define do
 
     factory :buyer_shipping_address do
-  
-      post_code { '1234567' }
+
+
+      user_id { 1 }
+      item_id { 1 }
+      
+      token { '1234567' }
+      
+      post_code { '123-4567' }
       prefecture_id { 1 }
       city { '東京都' }
       address { '中央区' }
       building_name { 'テックキャンプビル' }
       tell_number { '09012345678' }
+    
       
     end
   
