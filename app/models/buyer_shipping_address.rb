07697ea159ec8dbeb0665d39b57e4634a852c507
Shimshin21 <shin.11.pei@gmail.com>
2022-03-11 16:02:55 +0900

    修正①

--- app/models/buyer_shipping_address.rb
+++ app/models/buyer_shipping_address.rb
@@ -7,15 +7,13 @@ class BuyerShippingAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-
     validates :token
-
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :address
     validates :tell_number, format: { with: /\A[0-9]+\z/ }
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
   validates :tell_number, format: { with: /\A\d{10,11}\z/, message: 'of characters is invalid.' }
 
   def save
