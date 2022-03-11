58ed9d3aeaedd1d3d0edef1eb3e0b2ef85faa74a
Shimshin21 <shin.11.pei@gmail.com>
2022-03-11 17:02:37 +0900

    修正③

--- app/models/buyer_shipping_address.rb
+++ app/models/buyer_shipping_address.rb
@@ -13,13 +13,27 @@ class BuyerShippingAddress
     validates :address
     validates :tell_number, format: { with: /\A[0-9]+\z/ }
   end
-  validates :prefecture_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :tell_number, format: { with: /\A\d{10,11}\z/, message: 'of characters is invalid.' }
 
-  
+
   def save
     shipping_address = ShippingAddress.create(item_id: item_id, user_id: user_id)
     Buyer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name,
                  tell_number: tell_number, shipping_address_id: shipping_address.id)
   end
 end
+
+
+shimpei
+19881111
+
+
+test10@test
+qwe123
+
+
+test20@test
+qwe123
+
+
